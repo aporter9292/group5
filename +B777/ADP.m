d8dd30c873beda45fee0f5206c59b58f467390a4
SSS-Lee <io25228@bristol.ac.uk>
2026-03-03 14:50:14 +0000

    Updated by Sinatra for Code Integration v1.0
    Values changed for the T&W design at class-ii fidelity.

--- +B777/ADP.m
+++ +B777/ADP.m
@@ -26,15 +26,15 @@ classdef ADP < handle
     end
     % Aerodynamic
     properties
-        % ------------------------- geometry -------------------------
-        V_HT = 0.9; % Horizontal Tail Volume
+        % ------------------------ Empennage -------------------------
+        V_HT = 0.9; % Horizontal tail volume
         V_VT = 0.07; % Vertical tail volume
 
-        % --------------------- aero properties ----------------------
+        % -------------------------- Wing ----------------------------
         Cl_max = 1.5;   % airfoil amx Cl for wing
         
-        Delta_Cl_ld = 1; % Extra CL during landing
-        Delta_Cl_to = 0.8; % Extra CL at take-off
+        Delta_Cl_ld = 1.1; % Extra CL during landing
+        Delta_Cl_to = 0.6; % Extra CL at take-off
 
         CD_TO = 0.03;     % CD in ground run
         CL_TO = 0.8;      % CL during ground run        
@@ -44,8 +44,14 @@ classdef ADP < handle
 
         LD_c = 16;        % Lift to drag ratio in cruise
         LD_app = 10;      % Lift to drag ratio during landing
-        CD0 = 0.02;       % Zero-lift drag coefficent
+
+        CD0 = 0.016;      % Zero-lift drag coefficent
+        CD_flap_to = 0.013;
+        CD_gear = 0.017;
+        CDi = 0.033;
+
         e = 0.8;          % Oswald Efficency Factor
+        e_to_ld = 0.7;    % Oswald Efficency Factor at TO/Landing
     end
 
     % Sizing Flags (whether to Adjust certain values during sizing process)
@@ -54,17 +60,18 @@ classdef ADP < handle
         isSizeWing = true; % whether to size the wing
     end
 
-    % Concrete properties
+    % Wing & Empennage Structure
     properties
         Thrust;
 
         % planfrom specific
-        Span;
+        % Span = 64.8;
+        Span = 72.0;
         WingArea;
-        KinkPos;    % y position of wing kink 
-        WingPos;    % Wing position along fuselage
-        HtpPos;     % HTP pos along fuselage
-        VtpPos;     % VTP pos along fuselage
+        KinkPos = 10;    % y position of wing kink 
+        WingPos = 0.44*63.7;    % Wing position along fuselage
+        HtpPos = 0.85*63.7;     % HTP pos along fuselage
+        VtpPos = 0.82*63.7;     % VTP pos along fuselage
 
         Mstar = 0.935; % wing technology factor
 
@@ -85,7 +92,7 @@ classdef ADP < handle
     properties
         CockpitLength = 6;
         CabinRadius = 3.1;
-        CabinLength = 63.7 - 4 - 3.1*2*1.48;
+        CabinLength = 63.73 - 4 - 3.1*2*1.48;
     end
 
     methods
